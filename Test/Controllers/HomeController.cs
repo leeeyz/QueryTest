c13db24518a50e4f64950443611d3c2ab7c5a18b
lyz <ussrdprkor@aliyun.com>
2018-07-13 11:32:15 +0800

    提交

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Buy()
+        {
+            string guid = Guid.NewGuid().ToString();
+            if(MvcApplication.SubscribeInfo.IsFulled())
+            {
+                return new JsonResult() { Data = new { msg = "已经结束" }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+            else
+            {
+                bool result = MvcApplication.SubscribeInfo.Incr(guid);
+                if(result)
+                {
+                    return new JsonResult() { Data = new { msg = "成功" }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                }
+                else
+                {
+                    return new JsonResult() { Data = new { msg = "失败" }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                }
+            }
+        }
+    }
+}
---
+++ Test/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using MyHelper.Common;
+using MyHelper.Common.Log;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Test
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static SubscribeInfo SubscribeInfo = new SubscribeInfo(10);
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SubscribeInfo.Subscribe(async mqmsg=> {
+                await Task.Run(() => {
+                    LogHelper.LogInfo(mqmsg.Msg);
+                });
+            });
+
+            SubscribeInfo.ErrorHandle(async (dyn, expc, type) => {
+                await Task.Run(() => {
+                    LogHelper.LogError(JsonConvert.SerializeObject(dyn), expc);
+                });
+            });
+        }
+    }
+}
